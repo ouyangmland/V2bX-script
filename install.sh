ed215ac486f39589c7b22670a4f320d2afe8e55f
ouyangmland <ouyangmland@gmail.com>
2026-07-21 20:18:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -138,7 +138,7 @@ install_V2bX() {
             exit 1
         fi
         echo -e "检测到 V2bX 最新版本：${last_version}，开始安装"
-        wget --no-check-certificate -N --progress=bar -O /usr/local/V2bX/V2bX-linux.zip https://github.com/ouyangmland/V2bX-singbox/releases/download/${last_version}/V2bX-linux-arm64.zip
+        wget --no-check-certificate -N --progress=bar -O /usr/local/V2bX/V2bX-linux.zip https://github.com/ouyangmland/V2bX-singbox/releases/download/${last_version}/V2bX-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 V2bX 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
